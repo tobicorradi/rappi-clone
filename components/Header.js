a13a63da7236f08589957768663e8d022473f86b
tobicorradi <corraditobias@gmail.com>
2022-02-04 23:16:17 -0300

    Logo added

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,15 @@ import logo from '../public/images/logo.png'
 
 const Header = () => {
     return (
-        <header>
-            <Image src={logo} />
-            Soy un header
+        <header className="bg-white p-5">
+            <div className="relative flex items-center h-8 cursor-pointer">
+                <Image 
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Rappi_logo.svg/1200px-Rappi_logo.svg.png"
+                    layout="fill"
+                    objectFit="contain"
+                    objectPosition="left"
+                />
+            </div>
         </header>
     );
 };
