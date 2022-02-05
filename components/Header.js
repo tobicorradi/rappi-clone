99e828ed4bbe2d5f127b9c5c22201f54d2ca7b05
tobicorradi <corraditobias@gmail.com>
2022-02-04 22:38:49 -0300

    Header component added

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const Header = () => {
+    return (
+        <header>
+            <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Rappi_logo.svg/1200px-Rappi_logo.svg.png"/>
+            Soy un header
+        </header>
+    );
+};
+
+export default Header;
