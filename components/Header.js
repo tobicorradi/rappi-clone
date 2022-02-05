0a5ff9a58e33092125f9d2b840471b129d1454a6
tobicorradi <corraditobias@gmail.com>
2022-02-04 23:50:01 -0300

    Header styling first steps

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,8 @@
 import Image from "next/image";
-import logo from '../public/images/logo.png'
-
+import { SearchIcon, ShoppingCartIcon } from '@heroicons/react/solid'
 const Header = () => {
     return (
-        <header className="bg-white p-5">
+        <header className="bg-white p-5 grid">
             <div className="relative flex items-center h-8 cursor-pointer">
                 <Image 
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Rappi_logo.svg/1200px-Rappi_logo.svg.png"
@@ -12,6 +11,20 @@ const Header = () => {
                     objectPosition="left"
                 />
             </div>
+            <div>
+                <form className="flex items-center bg-slate-50 px-4 py-2 rounded text-md">
+                    <input
+                        className="flex-grow bg-transparent focus:outline-none" 
+                        type="text"
+                        placeholder="Buscar en Rappi"
+                    />
+                    <SearchIcon className="h-10 text-white bg-red-500 p-2 rounded-lg cursor-pointer" />
+                </form>
+                <button className="flex items-center bg-green-400 text-white p-2 cursor-pointer rounded-lg px-3 font-bold">
+                    <ShoppingCartIcon className="h-7 mr-2"/>
+                    <span>0</span>
+                </button>
+                </div>
         </header>
     );
 };
