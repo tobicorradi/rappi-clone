f4f0954a195ce5d277a40b2150b8af5bbc8f14ae
tobicorradi <corraditobias@gmail.com>
2022-02-05 01:09:07 -0300

    Nunito font added

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import Header from '../components/Header'
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Comforter+Brush&family=Nunito:wght@200;400;700&display=swap" rel="stylesheet" /> 
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
